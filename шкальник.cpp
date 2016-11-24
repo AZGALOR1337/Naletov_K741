f906f2ecfb1fa6cee3d61cf96026ad1580ca443b
AZGALOR1337 <denispetrov1998-2012@mail.ru>
2016-11-24 22:09:15 +0800

    Школьники

---
+++ шкальник.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std; 
+class st( 
+	int och[5]; 
+    char* group; 
+    char* FIO; 
+    public: 
+st(char* _name[],char* _group[]) { 
+FIO=_name; 
+group=_group; 
+char* getname(); 
+{ 
+return FIO; 
+} 
+char* getgroup(); 
+{ 
+return group; 
+} 
+}; 
+void main(){ 
+st: a("Vasya","3"); 
+cout<<s.getname(); 
+cout<<"\n";
+cout<<s.getgroup(); 
+cout<<"\n";
+system("pause"); 
+}
